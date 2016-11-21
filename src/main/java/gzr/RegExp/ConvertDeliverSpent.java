d461497f6064526c44000a0a4fcde7e78c8f42c9
zengrong.gzr <zengrong.gzr@alibaba-inc.com>
2016-11-21 10:25:56 +0800

    regExp

---
+++ src/main/java/gzr/RegExp/ConvertDeliverSpent.java
@@ -0,0 +1,32 @@
+package gzr.RegExp;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by gaozengrong on 16/11/21.
+ */
+public class ConvertDeliverSpent {
+    public static void main(String[] args) {
+        String a=" 180sfdsgdsagsdaf";
+        System.out.println(convertDeliverSpent(a));
+    }
+
+    private static String convertDeliverSpent(String param){
+        String regEx="[^0-9]";
+        Pattern p = Pattern.compile(regEx);
+        String num = p.matcher(param).replaceAll("").trim();
+        Long delivertime = Long.parseLong(num);
+        int hour = (int) (delivertime/60);
+        int minute = (int) (delivertime%60);
+        if (hour == 0){
+            return minute+"分钟送达";
+        }else {
+            if(minute == 0){
+                return hour+"小时送达";
+            }else {
+                return hour+"小时"+minute+"分钟送达";
+            }
+        }
+    }
+
+}
