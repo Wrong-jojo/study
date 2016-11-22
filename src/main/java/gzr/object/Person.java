fdc31e0f5ccab7ce347b51027aa1804d1f16664a
zengrong.gzr <zengrong.gzr@alibaba-inc.com>
2016-11-22 15:17:13 +0800

    序列化

---
+++ src/main/java/gzr/object/Person.java
@@ -0,0 +1,48 @@
+package gzr.object;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.io.Serializable;
+
+/**
+ * Created by gaozengrong on 16/11/22.
+ */
+public class Person implements Serializable {
+    private static final long serialVersionUID = -2569740845621051313L;
+    private int age;
+
+    private String name;
+
+    private String sex;
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+//        return String.valueOf(age) +"|" + String.valueOf(name) + "|" + String.valueOf(sex);
+    }
+}
