d0685de8813f6d65e52ee6fc415674b70c1615e4
zengrong.gzr <zengrong.gzr@alibaba-inc.com>
2016-11-29 16:45:38 +0800

    cglib代理

---
+++ src/main/java/gzr/Proxy/Cglib/CglibProxy.java
@@ -0,0 +1,44 @@
+package gzr.Proxy.Cglib;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * JDK的动态代理机制只能代理实现了接口的类，而不能实现接口的类就不能实现JDK的动态代理，
+ * cglib是针对类来实现代理的，他的原理是对指定的目标类生成一个子类，并覆盖其中方法实现增强，但因为采用的是继承，所以不能对final修饰的类进行代理。
+ * Created by gaozengrong on 16/11/29.
+ */
+public class CglibProxy implements MethodInterceptor{
+    private Object target;
+
+    /**
+     * 创建代理对象
+     *
+     * @param target
+     * @return
+     */
+    public Object getInstance(Object target) {
+        this.target = target;
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(this.target.getClass());
+        // 回调方法
+        enhancer.setCallback(this);
+        // 创建代理对象
+        return enhancer.create();
+    }
+
+    @Override
+    // 回调方法
+    public Object intercept(Object obj, Method method, Object[] args,
+                            MethodProxy proxy) throws Throwable {
+        System.out.println("事物开始");
+        proxy.invokeSuper(obj, args);
+        System.out.println("事物结束");
+        return null;
+
+
+    }
+}
