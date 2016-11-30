fa54fc486b6d3fb2cb5cf74acadeb6d4b0200ed7
高曾荣 <985076835@qq.com>
2016-11-30 11:49:25 +0800

    修改Dynamic和GBKToUTF8两个文件的编码

---
+++ src/main/java/gzr/tools/GBKToUTF8.java
@@ -0,0 +1,24 @@
+package gzr.tools;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.util.Collection;
+
+/**
+ * Created by gaozengrong on 16/11/30.
+ */
+public class GBKToUTF8 {
+    public static void main(String[] args) throws Exception {
+
+        String gbkDirPath = "/Users/gaozengrong/gzr/study/src";//GBK编码格式源码文件路径
+        String utf8DirPath = "/Users/gaozengrong/gzr/study/src";//转为UTF-8编码格式源码文件保存路径
+
+        @SuppressWarnings("unchecked")
+        Collection<File> gbkFileList =  FileUtils.listFiles(new File(gbkDirPath), new String[]{"java"}, true);//获取所有java文件
+        for (File gbkFile : gbkFileList) {
+            String utf8FilePath = utf8DirPath + gbkFile.getAbsolutePath().substring(gbkDirPath.length());//UTF-8编码格式文件保存路径
+            FileUtils.writeLines(new File(utf8FilePath), "UTF-8", FileUtils.readLines(gbkFile, "GBK"));//使用GBK编码格式读取文件，然后用UTF-8编码格式写入数据
+        }
+    }
+}
