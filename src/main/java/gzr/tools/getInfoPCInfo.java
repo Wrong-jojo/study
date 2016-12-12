78621756476afa4e10a03f89cf28237f36e89b6f
高曾荣 <zengrong.gzr@alibaba-inc.com>
2016-12-12 17:13:56 +0800

    获取pc info 比如用户名等

---
+++ src/main/java/gzr/tools/getInfoPCInfo.java
@@ -0,0 +1,22 @@
+package gzr.tools;
+
+import java.util.Map;
+
+/**
+ * 获取用户名等信息
+ * Created by gaozengrong on 16/12/12.
+ */
+public class getInfoPCInfo {
+    public static void main(String[] args) {
+        Map< String,String> map = System.getenv();
+        System.out.println(map);
+
+        /*windows*/
+        System.out.println(map.get("USERNAME"));//获取用户名
+        System.out.println(map.get("COMPUTERNAME"));//获取计算机名
+        System.out.println(map.get("USERDOMAIN"));//获取计算机域名
+
+        /*mac*/
+        System.out.println(map.get("USER"));
+    }
+}
