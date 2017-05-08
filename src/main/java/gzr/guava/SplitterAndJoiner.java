db31cd12a9a959ef651fb9063f956639509fed56
zengrong.gzr <zengrong.gzr@alibaba-inc.com>
2017-05-09 00:45:55 +0800

    Joiner userForNull添加

--- src/main/java/gzr/guava/SplitterAndJoiner.java
+++ src/main/java/gzr/guava/SplitterAndJoiner.java
@@ -15,27 +15,27 @@ public class SplitterAndJoiner {
         List<String> list = new ArrayList<String>();
         System.out.println();
         long startTime = System.currentTimeMillis();
-        StringToList();
+        stringToList();
         long endTime = System.currentTimeMillis();
         System.out.println("StringToList()函数运行时间： "+(endTime-startTime)+"ms");
         startTime = System.currentTimeMillis();
-        ListToString();
+        listToString();
         endTime = System.currentTimeMillis();
         System.out.println("ListToString()函数运行时间： "+(endTime-startTime)+"ms");
     }
 
-    private static void StringToList(){
+    private static void stringToList(){
         String str = "a, b, c";
         List<String> result = Splitter.on(",").trimResults().splitToList(str);
         System.out.println(result); //输出[a, b, c]
     }
 
-    private static void ListToString(){
+    private static void listToString(){
         List<String> list = new ArrayList<String>();
         list.add("a");
         list.add("b");
         list.add("c");
-        String str = Joiner.on(",").join(list);
+        String str = Joiner.on(",").useForNull("").join(list);
         System.out.println(str);  //输出a,b,c
     }
 
