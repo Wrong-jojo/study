aa4d4a7f9af8c40045790d62499425e820e181ae
zengrong.gzr <zengrong.gzr@alibaba-inc.com>
2017-05-17 17:09:15 +0800

    String to Map by Splitter

--- src/main/java/gzr/guava/SplitterAndJoiner.java
+++ src/main/java/gzr/guava/SplitterAndJoiner.java
@@ -2,6 +2,7 @@ package gzr.guava;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
@@ -14,23 +15,31 @@ public class SplitterAndJoiner {
     public static void main(String[] args) {
         List<String> list = new ArrayList<String>();
         System.out.println();
+
         long startTime = System.currentTimeMillis();
         stringToList();
         long endTime = System.currentTimeMillis();
-        System.out.println("StringToList()函数运行时间： "+(endTime-startTime)+"ms");
+        System.out.println("StringToList()函数运行时间： " + (endTime - startTime) + "ms");
+
         startTime = System.currentTimeMillis();
         listToString();
         endTime = System.currentTimeMillis();
-        System.out.println("ListToString()函数运行时间： "+(endTime-startTime)+"ms");
+        System.out.println("ListToString()函数运行时间： " + (endTime - startTime) + "ms");
+        startTime = System.currentTimeMillis();
+
+        startTime = System.currentTimeMillis();
+        stringToMap();
+        endTime = System.currentTimeMillis();
+        System.out.println("stringToMap()函数运行时间： " + (endTime - startTime) + "ms");
     }
 
-    private static void stringToList(){
+    private static void stringToList() {
         String str = "a, b, c";
         List<String> result = Splitter.on(",").trimResults().splitToList(str);
         System.out.println(result); //输出[a, b, c]
     }
 
-    private static void listToString(){
+    private static void listToString() {
         List<String> list = new ArrayList<String>();
         list.add("a");
         list.add("b");
@@ -39,5 +48,10 @@ public class SplitterAndJoiner {
         System.out.println(str);  //输出a,b,c
     }
 
+    private static void stringToMap() {
+        String s = "name=gaozengrong,age=26,height=178";
+        Map<String, String> map = Splitter.on(',').withKeyValueSeparator('=').split(s);
+        System.out.println(map);
+    }
 
 }
