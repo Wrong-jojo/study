8dafefc2268e6d79cbfdf0db31ef182dbf5eed07
zengrong.gzr <zengrong.gzr@alibaba-inc.com>
2017-07-04 11:28:56 +0800

    分组并发

--- src/main/java/gzr/guava/Partition.java
+++ src/main/java/gzr/guava/Partition.java
@@ -1,6 +1,11 @@
 package gzr.guava;
 
 import java.util.List;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.google.common.collect.Lists;
 
@@ -11,7 +16,7 @@ import com.google.common.collect.Lists;
  * @date 2017/07/04
  */
 public class Partition {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         List<Long> list = Lists.newArrayList();
         for (int i = 0; i < 101; i++) {
             list.add((long)i);
@@ -19,8 +24,26 @@ public class Partition {
         //分组 每组10个
         List<List<Long>> partitions = Lists.partition(list, 10);
         System.out.println("partitions size is " + partitions.size());
-        for (List<Long> partition : partitions) {
-            System.out.println(partition.get(0));
-        }
+
+        ExecutorService executor = Executors.newFixedThreadPool(partitions.size());
+        CompletionService<Long> cs = new ExecutorCompletionService<>(executor);
+
+        System.out.println("print all data concurrently");
+        partitions.forEach(partition -> {
+            cs.submit(() -> {
+                partition.forEach(System.out::println);
+                return partition.get(0);
+            });
+        });
+
+        Thread.sleep(1000);
+        System.out.println("print concurrent result");
+        partitions.forEach(partition -> {
+            try {
+                System.out.println(cs.take().get());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
