d3428185ef53c118ee353142bc916403d03a66b6
zengrong.gzr <zengrong.gzr@alibaba-inc.com>
2017-07-21 14:39:48 +0800

    当key为数字时,json序列化后出错 使用SerializerFeature.WriteNonStringKeyAsString

--- src/main/java/gzr/gsonAndJson/FastJsonHelper.java
+++ src/main/java/gzr/gsonAndJson/FastJsonHelper.java
@@ -1,6 +1,10 @@
 package gzr.gsonAndJson;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 
 /**
  * @author zengrong.gzr
@@ -18,5 +22,22 @@ public class FastJsonHelper {
         Girl copyGirl = JSON.parseObject(girlStr, Girl.class);
         System.out.println("origin girl: " + girl);
         System.out.println("copy girl: " + copyGirl);
+
+        whenKeyIsNum();
+    }
+
+    /**
+     * 当key为数字时,json序列化后出错
+     * https://github.com/alibaba/fastjson/issues/66
+     */
+    public static void whenKeyIsNum(){
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1512, "手机");
+        map.put(1101, "笔记本电脑");
+        //输出  {1101:"笔记本电脑",1512:"手机"}
+        System.out.println(JSON.toJSONString((map), SerializerFeature.QuoteFieldNames));
+        //输出  {"1512":"手机","1101":"笔记本电脑"}
+        System.out.println(JSON.toJSONString((map), SerializerFeature.WriteNonStringKeyAsString));
+
     }
 }
