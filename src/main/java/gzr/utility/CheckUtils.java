ed3fbc3a5b5ff6a029703826d925dfcf16439e1a
zengrong.gzr <zengrong.gzr@alibaba-inc.com>
2017-08-24 10:38:25 +0800

    optional

--- src/main/java/gzr/utility/CheckUtils.java
+++ src/main/java/gzr/utility/CheckUtils.java
@@ -1,8 +1,19 @@
 package gzr.utility;
 
+import java.util.Optional;
+
 /**
  * @author zengrong.gzr
  * @date 2017/08/13
  */
 public class CheckUtils {
+    public static boolean notNullAndGreaterThanZero(Integer i) {
+        return Optional.ofNullable(i).orElse(0) > 0;
+        //return i != null && i > 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Optional.ofNullable(null));
+        System.out.println(notNullAndGreaterThanZero(1));
+    }
 }
