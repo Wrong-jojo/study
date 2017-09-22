c9e79d981186faa16df15b3e6154fce609e5d8cb
zengrong.gzr <zengrong.gzr@alibaba-inc.com>
2017-09-22 19:48:08 +0800

    比较两个时间是否间隔大于一天

--- src/main/java/gzr/date/DateUtil.java
+++ src/main/java/gzr/date/DateUtil.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by gaozengrong on 16/11/22.
@@ -18,18 +19,19 @@ public class DateUtil {
 
         Date startTime = new Date();
         Date endTime = new Date(startTime.getTime() + 20 * 60 * 60 * 1000);
-        System.out.println("在" + startTime + "和" + endTime + "区间内的整点时间有：" + getWholePointInInterval(startTime,endTime));
+        System.out.println("在" + startTime + "和" + endTime + "区间内的整点时间有：" + getWholePointInInterval(startTime, endTime));
     }
 
     /**
      * //获取第二天24点的时间
+     *
      * @return
      */
-    private static Date getTomorrow24(){
+    private static Date getTomorrow24() {
         Calendar endTime = Calendar.getInstance();
-        endTime.add(java.util.Calendar.DATE,1);
+        endTime.add(java.util.Calendar.DATE, 1);
         endTime.set(java.util.Calendar.HOUR_OF_DAY, 24);
-//        endTime.set(java.util.Calendar.HOUR, 24);
+        //        endTime.set(java.util.Calendar.HOUR, 24);
         endTime.set(java.util.Calendar.MINUTE, 0);
         endTime.set(java.util.Calendar.SECOND, 0);
         endTime.set(java.util.Calendar.MILLISECOND, 0);
@@ -38,10 +40,11 @@ public class DateUtil {
 
     /**
      * Date转换为Calendar
+     *
      * @param date
      * @return
      */
-    private static Calendar convertDateToCalendar(Date date){
+    private static Calendar convertDateToCalendar(Date date) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         return calendar;
@@ -49,33 +52,62 @@ public class DateUtil {
 
     /**
      * 获取下一个整点的时间
+     *
      * @param date
      * @return
      */
-    private static Date getWholePoint(Date date){
+    private static Date getWholePoint(Date date) {
         Calendar calendar = convertDateToCalendar(date);
-        calendar.add(Calendar.HOUR_OF_DAY,1);
-        calendar.set(Calendar.MINUTE,0);
-        calendar.set(Calendar.SECOND,0);
-        calendar.set(Calendar.MILLISECOND,0);
+        calendar.add(Calendar.HOUR_OF_DAY, 1);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
         return calendar.getTime();
     }
 
     /**
      * 获取时间区间内的整点时间
+     *
      * @param startTime
      * @param endTime
      * @return
      */
-    private static List<String> getWholePointInInterval(Date startTime, Date endTime){
+    private static List<String> getWholePointInInterval(Date startTime, Date endTime) {
         Date wholePoint = getWholePoint(startTime);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHH");
         List<String> result = new ArrayList<>();
-        while (wholePoint.before(endTime)){
+        while (wholePoint.before(endTime)) {
             result.add(sdf.format(wholePoint));
             wholePoint = new Date(wholePoint.getTime() + 60 * 60 * 1000);
         }
         return result;
+    }
+
+    /**
+     * 比较后一个时间是否大于前一个时间超过一天
+     *
+     * @param before
+     * @param after
+     * @return
+     */
+    private static Boolean compareTwoTime1(Date before, Date after) {
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.setTime(before);
+        calendar1.add(Calendar.DATE, 1);
+        Calendar calendar2 = Calendar.getInstance();
+        calendar2.setTime(after);
+        return calendar1.before(calendar2);
+    }
+
+    /**
+     * 比较后一个时间是否大于前一个时间超过一天
+     *
+     * @param before
+     * @param after
+     * @return
+     */
+    private static Boolean compareTwoTime2(Date before, Date after) {
+        return after.getTime() - before.getTime() > TimeUnit.DAYS.toMillis(1);
     }
 
 }
