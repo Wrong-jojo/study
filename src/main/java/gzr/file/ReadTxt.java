fe1b616a2b80e1a13e0466f15b1d35490776553f
zengrong.gzr <zengrong.gzr@alibaba-inc.com>
2017-12-06 16:12:11 +0800

    read txt

--- src/main/java/gzr/file/ReadTxt.java
+++ src/main/java/gzr/file/ReadTxt.java
@@ -1,10 +1,10 @@
 package gzr.file;
 
-import com.google.common.base.Splitter;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.List;
+
+import com.google.common.base.Splitter;
 
 /**
  * Created by gaozengrong on 16/11/21.
@@ -17,13 +17,35 @@ public class ReadTxt {
             s = input.readLine();
             List<String> itemIds = Splitter.on(",").splitToList(s.trim());
             System.out.println(itemIds.size());
-//            while((s = input.readLine())!=null){ //判断是否读到了最后一行
-//                String info[] = s.split("\t");
-//                System.out.println("门店id:"+info[0]+"活动id:"+info[1]+"门店类型:"+info[2]+"添加/删除:"+info[3]);
-//            }
+            //            while((s = input.readLine())!=null){ //判断是否读到了最后一行
+            //                String info[] = s.split("\t");
+            //                System.out.println("门店id:"+info[0]+"活动id:"+info[1]+"门店类型:"+info[2]+"添加/删除:"+info[3]);
+            //            }
             input.close();
         } catch (Exception ignored) {
 
         }
+    }
+
+    /**
+     * 加载文件
+     *
+     * @param filePath 文件路径
+     * @return 文件所有内容
+     */
+    public static StringBuilder loadFile(String filePath) {
+        StringBuilder builder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String input;
+            while ((input = reader.readLine()) != null) {
+                builder.append(input);
+                builder.append('\n');
+            }
+        } catch (Exception ignored) {
+
+        }
+        return builder;
+
     }
 }
